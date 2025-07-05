a63f4364719f19ebea40561409635540f9cf9e78
Chen Enola <chensofia0214@gmail.com>
2025-07-05 21:30:52 +0800

    Initial Vue+Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        // 保留 Vue 的顏色變數
+        'vt-c-white': '#ffffff',
+        'vt-c-white-soft': '#f8f8f8',
+        'vt-c-white-mute': '#f2f2f2',
+        'vt-c-black': '#181818',
+        'vt-c-black-soft': '#222222',
+        'vt-c-black-mute': '#282828',
+        'vt-c-indigo': '#2c3e50',
+      },
+    },
+  },
+  plugins: [],
+}
